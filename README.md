d0306c6acd6ddce4503160dbd3b07a652029c010
Leon Silas <leonsilasdev@gmail.com>
2021-06-30 05:22:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# This is a title
+## This is a subtitle
+### This is a sub subtitle
+
+Below is a demonstration of syntax in Read Me files.  Go ahead and use the different types as you want to make your file for this project!
+
+This is `text` under this title.
+>Setting a new line does create a new line.
+You want **bold**? *Italic*? **Bold and _italic_**?
+
+- How about a list?
+- We do like lists.
+1. Even numbered ones!
+2. No matter how short or long!
+    - Though I will say, nesting them too much can be a hassle.
+
+```
+If you really want to make even more alterations you can make blocks.
+I hope all of this helps you!
+Thanks for joining me on this journey and I hope you enjoy [my work](https://github.com/leonsilas)!
+```
+
+# P.S.
+
+- [] Remind me to fix code bugs...
---
+++ scripts.js
@@ -0,0 +1,61 @@
+function showPopup() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+function hide() {
+    document.getElementById('toolbar').style.display = 'none';
+}
+
+function reveal() {
+    document.getElementById('start').style.visibility = 'visible';
+}
+
+function block() {
+    document.getElementById('haha').style.visibility = 'visible';
+}
+
+function awwrats() {
+    document.getElementById('haha').style.visibility = 'hidden';
+    document.getElementById('start').style.visibility = 'visible';
+}
+
+function rateMe1() {
+    document.getElementById('star1').className = "fa fa-star checked";
+    document.getElementById('star2').className = "fa fa-star";
+    document.getElementById('star3').className = "fa fa-star";
+    document.getElementById('star4').className = "fa fa-star";
+    document.getElementById('star5').className = "fa fa-star";
+}
+
+function rateMe2() {
+    document.getElementById('star1').className = "fa fa-star checked";
+    document.getElementById('star2').className = "fa fa-star checked";
+    document.getElementById('star3').className = "fa fa-star";
+    document.getElementById('star4').className = "fa fa-star";
+    document.getElementById('star5').className = "fa fa-star";
+}
+
+function rateMe3() {
+    document.getElementById('star1').className = "fa fa-star checked";
+    document.getElementById('star2').className = "fa fa-star checked";
+    document.getElementById('star3').className = "fa fa-star checked";
+    document.getElementById('star4').className = "fa fa-star";
+    document.getElementById('star5').className = "fa fa-star";
+}
+
+function rateMe4() {
+    document.getElementById('star1').className = "fa fa-star checked";
+    document.getElementById('star2').className = "fa fa-star checked";
+    document.getElementById('star3').className = "fa fa-star checked";
+    document.getElementById('star4').className = "fa fa-star checked";
+    document.getElementById('star5').className = "fa fa-star";
+}
+
+function rateMe5() {
+    document.getElementById('star1').className = "fa fa-star checked";
+    document.getElementById('star2').className = "fa fa-star checked";
+    document.getElementById('star3').className = "fa fa-star checked";
+    document.getElementById('star4').className = "fa fa-star checked";
+    document.getElementById('star5').className = "fa fa-star checked";
+}
